064a6e073bafacddacfc47a746fa85fa111f6ce9
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-05-09 20:06:51 +0300

    feat: add main page using Bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -10,4 +10,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
